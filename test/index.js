1e23d150a51cbcdcf702309a7b546937011e0361
Alister Lewis-Bowen <alister@different.com>
2012-09-17 22:10:09 -0400

    refactor test and make onStart reachable

--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
 !function () {
 
-    var config = require('./config.json'),
+    var tty = require('tty'),
+        config = require('./config.json'),
         skypeChat = require('../index'),
         skype = new skypeChat(config.account),
         session = null,
         count = 0;
 
-    skype.onStart = function () {
+    onStart = function () {
         log ('Session started and listening for messages.');
 
         session.on('stopped', function () {
@@ -32,7 +33,14 @@
         console.log('[skype_chat test] %s', msg);
     }
 
+    process.stdin.on('keypress', function(chunk, key) {
+        if (key && jey.name === 'c' && key.ctrl) {
+            skype.stop();
+        }
+    });
+
     log('Start a Skype chat session...');
-    session = skype.start();
+    session = skype.start(onStart);
+    process.stdin.setRawMode();
 
 }();
